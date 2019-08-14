9076a420789b9106a97907b4614caba3f6a388a3
yogita <yogitaghai728@gmail.com>
2019-08-15 11:25:10 +1200

    This is my first commit

--- aboutus.php
+++ aboutus.php
@@ -140,7 +140,7 @@ ul li {list-style:none;}
 <!--navbar ends-->
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
   
-    <a class="navbar-brand" href="index.php"><span style="color:green;font-family: 'Permanent Marker', cursive;">Food Hunt</span></a>
+    <a class="navbar-brand" href="index.php"><span style="color:green;font-family: 'Permanent Marker', cursive;">The Good Cheff</span></a>
     <?php
 	if(!empty($cust_id))
 	{
